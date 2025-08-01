7af1b45beeabce1084b87c34168977706eb2a714
Agraj-Yadav <agrajy1@gmail.com>
2025-08-01 11:09:04 +0530

    Update storbrr.py

--- storbrr.py
+++ storbrr.py
@@ -1,4 +1,4 @@
 import pandas as pd
-tickerframe = pd.read_excel(r"C:\Users\cbec\Downloads\Yahoo-Ticker-Symbols-September-2017\Yahoo Ticker Symbols - September 2017.xlsx")
+tickerframe = pd.read_excel(r"Yahoo Ticker Symbols - September 2017.xlsx")
 tickerframe = tickerframe.iloc[3:].dropna(subset=['Yahoo Stock Tickers'])
 print("Kay")
